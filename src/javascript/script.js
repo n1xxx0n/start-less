c480d0fe0151191484a4cbdf1f99acfe05b87c47
adamcovert <adamcovert.shifted@gmail.com>
2017-09-21 17:41:22 +0300

    Added bootstrap 3.3.7

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,6 +1,7 @@
 //= ../../node_modules/jquery/dist/jquery.min.js
 //= ../../node_modules/jquery-migrate/dist/jquery-migrate.min.js
 //= ../../node_modules/svg4everybody/dist/svg4everybody.min.js
+// //= ../..//node_modules/owl.carousel/dist/owl.carousel.min.js
 
 $(document).ready(function() {
   svg4everybody();
