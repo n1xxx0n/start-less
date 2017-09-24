834b9a8d4d1f69bbc1169b2491f1c86426157711
adamcovert <adamcovert.shifted@gmail.com>
2017-09-24 12:28:22 +0300

    Updated

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,7 +1,19 @@
 //= ../../node_modules/jquery/dist/jquery.min.js
 //= ../../node_modules/jquery-migrate/dist/jquery-migrate.min.js
 //= ../../node_modules/svg4everybody/dist/svg4everybody.min.js
+
+// List of Bootstrap JS Components
+// //= ../../node_modules/bootstrap/js/transition.js
+// //= ../../node_modules/bootstrap/js/modal.js
+
+// Responsive Carousel Slider
 // //= ../..//node_modules/owl.carousel/dist/owl.carousel.min.js
+
+// Rellax – Plagin For Parallax Effects
+// //= ../../node_modules/rellax/rellax.min.js
+
+// AOS – Animate on Scroll Library
+// //= ../../node_modules/aos/dist/aos.js
 
 $(document).ready(function() {
   svg4everybody();
